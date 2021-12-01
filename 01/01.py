5276ca33dedc1c4df83820b7d40a1e5f7305f2e9
mchlr <mrenn2000@yahoo.de>
2021-12-01 23:16:02 +0100

    Cleanup

--- 01/01.py
+++ 01/01.py
@@ -5,31 +5,14 @@ with open("input") as file:
 
     lastEntry = None
     increases = 1
-    decreases = 0
-    
-    print(len(content))
-
     for i in range(len(content)):
         if i == 0:
             continue
         else:
             if content[i] > content[i-1]:
                 increases += 1
-            else:
-                decreases += 1
-                print(f"{content[i]} < {content[i-1]} ?")
 
-    # for entry in content:
-    #     if lastEntry is not None:
-    #         if entry > lastEntry:
-    #             increases += 1
-    #     lastEntry = entry
-    
-print("Me")
-print(increases)
 
-print("decreases")
-print(decreases)
+print(f"Solution: {increases}")
 
-print(decreases + increases)
 
