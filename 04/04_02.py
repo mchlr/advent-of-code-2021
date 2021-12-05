bd10f99f6c4af3316ff4d8433d9852fa2a1abcfd
mchlr <mrenn2000@yahoo.de>
2021-12-05 14:08:51 +0100

    Add day 04

--- 04/04_02.py
+++ 04/04_02.py
@@ -18,7 +18,7 @@ def has_bingo(board, match):
 
     # col wise bingo
     for col_idx in range(board.shape[1]):
-        col = get_row_as_list(board, col_idx)
+        col = get_col_as_list(board, col_idx)
         if sum(np.isin(col, match)) == len(col):
             return True
     return False
@@ -30,6 +30,10 @@ def get_mask(board, match):
         row = get_row_as_list(board, row_idx)
         mask.append([0 if val in match else 1 for val in row])
     return np.matrix(mask)
+
+
+def rekreate_match(last_num, all):
+    return all[:(all.index(last_num)+1)]
  
 
 def solve():
@@ -50,15 +54,38 @@ def solve():
 
         # Convert to np.matrix
         bingos = [np.matrix(bb) for bb in bingos]
+        # for later matching
+        bingos_lut = [board for board in bingos]
+        winners = []
 
         for i in range(len(draws)):
-            match = np.array(draws[:i])
-            for board in bingos:
-                if has_bingo(board, match):
-                    mask = get_mask(board, match)
-                    mat_sum = sum(np.asarray(np.multiply(mask, board)).reshape(-1))
-                    return mat_sum * match[-1:]
+            match = np.array(draws[:i+1])
+            tmp_winners = []
 
+            # Determine last winning board
+            if len(match) == len(draws):
+                w_obj = winners[-1:][0]
+                # last board wins!
+                w_board = bingos_lut[w_obj["index"]]
+                w_match = rekreate_match(w_obj["last_match"][0], match.tolist())
+
+                # Get marked/unmarked as 0/1 matrix
+                mask = get_mask(w_board, w_match)
+                mat_sum = sum(np.asarray(np.multiply(mask, w_board)).reshape(-1))
+
+                return mat_sum * w_obj["last_match"][0]
+
+            # Check for other winners
+            for idx, board in enumerate(bingos):
+                if type(board) is not list:
+                    if has_bingo(board, match):
+                            tmp_winners.append({"index": idx, "last_match":match[-1:]})
+            
+            # Remove winners
+            for i in tmp_winners:
+                bingos[i["index"]] = []
+            winners.extend(tmp_winners)
+            
 
 print("Starting!")
 print(f"Solution: {solve()}")
